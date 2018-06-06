709ddaef7705129fd08faf9bc85d8bd8ea8253f9
root <root@ubuntu>
2018-06-06 12:35:08 +0800

    初始化

---
+++ 67-ed-line-editor-bonus/src/main/java/eDLineEditor/EDLineEditor.java
@@ -0,0 +1,19 @@
+package eDLineEditor;
+
+public class EDLineEditor {
+	
+	/**
+	 * 接收用户控制台的输入，解析命令，根据命令参数做出相应处理。
+	 * 不需要任何提示输入，不要输出任何额外的内容。
+	 * 输出换行时，使用System.out.println()。或者换行符使用System.getProperty("line.separator")。
+	 * 
+	 * 待测方法为public static void main(String[] args)方法。args不传递参数，所有输入通过命令行进行。
+	 * 方便手动运行。
+	 * 
+	 * 说明：可以添加其他类和方法，但不要删除该文件，改动该方法名和参数，不要改动该文件包名和类名
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+}
