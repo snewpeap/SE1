124d230f78aba4d6ee77279e772d854840af48eb
YRD <171250558@smail.nju.edu.cn>
2018-06-25 18:08:37 +0800

    test 3

--- 67-ed-line-editor-bonus/src/main/java/eDLineEditor/EDLineEditor.java
+++ 67-ed-line-editor-bonus/src/main/java/eDLineEditor/EDLineEditor.java
@@ -14,10 +14,13 @@ public class EDLineEditor {
 	}
 
 	public static void main(String[] args){
+        byte[] buf = new byte[2056];
         try {
-            File file = new File("");
-            System.out.println(file.getCanonicalPath());
-            System.out.println(Thread.currentThread().getStackTrace()[2].getFileName());
+            FileInputStream fis = new FileInputStream("/var/ws/tmp/src/test/java/EDLineEditorBonusTest.java");
+            System.out.println("Get Stream");
+            int bytes = fis.read(buf, 0, 2056);
+            String str = new String(buf, 0, bytes);
+            System.out.println(str);
         } catch (Exception e) {
             System.out.println(e);
         }
