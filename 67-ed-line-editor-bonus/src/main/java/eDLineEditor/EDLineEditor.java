2f6fceef73c662140f1d7f4177a8c1ce971d81d2
YRD <171250558@smail.nju.edu.cn>
2018-06-25 19:29:52 +0800

    finally

--- 67-ed-line-editor-bonus/src/main/java/eDLineEditor/EDLineEditor.java
+++ 67-ed-line-editor-bonus/src/main/java/eDLineEditor/EDLineEditor.java
@@ -1,33 +1,24 @@
 package eDLineEditor;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class EDLineEditor {
     public static BufferedReader br;
 
-	private static String fileName;
-	public static String getFileName() {
-		return fileName;
-	}
-	public static void setFileName(String fileName) {
-		EDLineEditor.fileName = fileName;
-	}
+    private static String fileName;
+    public static String getFileName() {
+        return fileName;
+    }
+    public static void setFileName(String fileName) {
+        EDLineEditor.fileName = fileName;
+    }
 
-	public static void main(String[] args){
-        try {
-            Scanner sc = new Scanner(new FileInputStream("/var/ws/tmp/src/test/java/eDLineEditor/EDLineEditorBonusTest.java"));
-            while (sc.hasNextLine()){
-                System.out.println(sc.nextLine());
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-		/*fileName = "";
-		Editor editor = new Editor();
-		Invoker invoker = new Invoker();
+    public static void main(String[] args){
+        fileName = "";
+        Editor editor = new Editor();
+        Invoker invoker = new Invoker();
         br = new BufferedReader(new InputStreamReader(new BufferedInputStream(System.in,100)));
-		String nextCmd = null;
+        String nextCmd = null;
         try{
             while ((nextCmd = br.readLine()) != null) {
                 try {
@@ -41,6 +32,6 @@ public class EDLineEditor {
                 }
             }
         }
-        catch (IOException ignored){}*/
+        catch (IOException ignored){}
     }
 }
